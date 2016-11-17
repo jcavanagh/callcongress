66cf2fd89da6c7fd0329319c709f36379ea0c57a
Joe Cavanagh <jcavanagh@apple.com>
2016-11-16 20:38:07 -0800

    Twilio basic endpoint

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fs from 'fs';
 import proxy from 'http-proxy-middleware';
+import twilio from 'twilio';
 
 const app = express();
 const isProd = process.env.NODE_ENV === 'production';
@@ -15,7 +16,43 @@ if(!isProd) {
       '^/proxy/gov/' : '/'
     }
   }));
+
+  app.use('/proxy/api/', proxy({
+    target: 'http://localhost:3001/',
+    changeOrigin: false,
+    pathRewrite: {
+      '^/proxy/api/' : '/api/'
+    }
+  }));
 }
+
+//Twilio
+const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+app.post('/api/call', (req, resp) => {
+  twilioClient.makeCall({
+    to: process.env.TWILIO_TEST_NUMBER,
+    from: process.env.TWILIO_NUMBER,
+    url: `http://${req.headers.host}/api/call/outbound`
+  }, (err, respData) => {
+    if(err) {
+      resp.send({
+        message: err,
+        response: respData
+      });
+    } else {
+      resp.send({
+        message: 'Call initiated',
+        response: respData
+      });
+    }
+  });
+});
+
+app.post('/api/call/outbound', (req, res) => {
+  res.type('text/xml');
+  res.send(`<?xml version="1.0" encoding="UTF-8"?><Response><Say voice="alice">Hey neat, it works</Say><Dial>${process.env.TWILIO_TEST_NUMBER}</Dial></Response>`);
+});
 
 app.listen(port, () => {
   console.log(`Backend listening on ${port}`);
--- src/components/congress/RepProfile.js
+++ src/components/congress/RepProfile.js
@@ -1,44 +1,51 @@
 import React from 'react';
+import axios from 'axios';
 
 import FontIcon from 'material-ui/FontIcon';
 import Paper from 'material-ui/Paper';
 
-export default function RepProfile(props) {
-  const member = props.member;
-  const leadershipRole = member.leadership_role ? <span>{member.leadership_role}<br/></span> : null;
-  const name = `${member.title}. ${member.first_name} ${member.last_name}`;
+export default class RepProfile extends React.Component {
+  call = (phone) => {
+    axios.post('/api/call');
+  }
 
-  return (
-    <div style={{display: 'inline-block', textAlign: 'center'}}>
-      <Paper zDepth={2} style={{ width: 300, padding: '1px 20px 20px 20px', margin: 20 }}>
-        <p style={{fontSize: 24}}>{leadershipRole}{name}<br/>{`(${member.party} - ${member.state})`}</p>
-        <Paper zDepth={3} circle={true} style={{height: 150, width: 150, margin: 'auto', textAlign: 'center', overflow:'hidden'}}>
-          <img
-            alt={name}
-            src={`https://theunitedstates.io/images/congress/450x550/${member.bioguide_id}.jpg`}
-            style={{width:'100%', height:'110%'}}
-          />
-        </Paper>
-        <div style={{marginTop: 20, cursor: 'pointer'}}>
-          <Paper zDepth={2} circle={true} style={{width: 60, height: 60, margin: 'auto', backgroundColor: '#449d44'}}>
-            <FontIcon className="fa fa-phone" style={{top: 18, color: 'white'}} />
+  render() {
+    const member = this.props.member;
+    const leadershipRole = member.leadership_role ? <span>{member.leadership_role}<br/></span> : null;
+    const name = `${member.title}. ${member.first_name} ${member.last_name}`;
+
+    return (
+      <div style={{display: 'inline-block', textAlign: 'center'}}>
+        <Paper zDepth={2} style={{ width: 300, padding: '1px 20px 20px 20px', margin: 20 }}>
+          <p style={{fontSize: 24}}>{leadershipRole}{name}<br/>{`(${member.party} - ${member.state})`}</p>
+          <Paper zDepth={3} circle={true} style={{height: 150, width: 150, margin: 'auto', textAlign: 'center', overflow:'hidden'}}>
+            <img
+              alt={name}
+              src={`https://theunitedstates.io/images/congress/450x550/${member.bioguide_id}.jpg`}
+              style={{width:'100%', height:'110%'}}
+            />
           </Paper>
-        </div>
-        <div style={{marginTop: 20}}>
-          <a title="Email" href={`mailto:${member.oc_email}`} target="_blank">
-            <FontIcon className="fa fa-envelope" style={{display: 'inline-block', marginRight: 20}} />
-          </a>
-          <a title="Facebook" href={`https://www.facebook.com/${member.facebook_id}`} target="_blank">
-            <FontIcon className="fa fa-facebook-square" style={{display: 'inline-block', marginRight: 20}} />
-          </a>
-          <a title="Twitter" href={`https://www.twitter.com/${member.twitter_id}`} target="_blank">
-            <FontIcon className="fa fa-twitter-square" style={{display: 'inline-block', marginRight: 20}} />
-          </a>
-          <a title="Website" href={`${member.website}`} target="_blank">
-            <FontIcon className="fa fa-globe" style={{display: 'inline-block'}} />
-          </a>
-        </div>
-      </Paper>
-    </div>
-  );
-};
+          <div style={{marginTop: 20, cursor: 'pointer'}} onClick={this.call.bind(this, member.phone)}>
+            <Paper zDepth={2} circle={true} style={{width: 60, height: 60, margin: 'auto', backgroundColor: '#449d44'}}>
+              <FontIcon className="fa fa-phone" style={{top: 18, color: 'white'}} />
+            </Paper>
+          </div>
+          <div style={{marginTop: 20}}>
+            <a title="Email" href={`mailto:${member.oc_email}`} target="_blank">
+              <FontIcon className="fa fa-envelope" style={{display: 'inline-block', marginRight: 20}} />
+            </a>
+            <a title="Facebook" href={`https://www.facebook.com/${member.facebook_id}`} target="_blank">
+              <FontIcon className="fa fa-facebook-square" style={{display: 'inline-block', marginRight: 20}} />
+            </a>
+            <a title="Twitter" href={`https://www.twitter.com/${member.twitter_id}`} target="_blank">
+              <FontIcon className="fa fa-twitter-square" style={{display: 'inline-block', marginRight: 20}} />
+            </a>
+            <a title="Website" href={`${member.website}`} target="_blank">
+              <FontIcon className="fa fa-globe" style={{display: 'inline-block'}} />
+            </a>
+          </div>
+        </Paper>
+      </div>
+    );
+  }
+}
